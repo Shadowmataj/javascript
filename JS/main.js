76e9f822e7beb3c2a15a78101d737badbe5066b0
Christian . <christianmataj1@gmail.com>
2024-01-28 22:22:45 -0600

    Sin cambios

--- JS/main.js
+++ JS/main.js
@@ -102,68 +102,72 @@ const modificarInformacion = () => {
     let index = parseInt(prompt("Indique el Index del producto que desea modificar: "))
     let cambios = parseInt(prompt("El producto tiene las siguientes propiedades: \n1.- Instrumento: " + productos[index].instrumento + "\n2.-Modelo: " + productos[index].modelo + "\n3.-Marca: " + productos[index].marca + "\n4.-Material: " + productos[index].material + "\n5.-Color: " + productos[index].color + "\n6.-Cuerdas: " + productos[index].cuerdas + "\n¿Cuántas propiedades del producto desea cambiar?"))
     for (let i = 0; i < cambios; i++) {
-        let propiedad = prompt("El producto tiene las siguientes propiedades: \n1.- Instrumento: " + productos[index].instrumento + "\n2.-Modelo: " + productos[index].modelo + "\n3.-Marca: " + productos[index].marca + "\n4.-Material: " + productos[index].material + "\n5.-Color: " + productos[index].color + "\n6.-Cuerdas: " + productos[index].cuerdas + "\nIndique la propiedad que desea modificar del producto").toLowerCase()
-        let nuevaPropiedad = prompt("El producto tiene las siguientes propiedades: \n1.- Instrumento: " + productos[index].instrumento + "\n2.-Modelo: " + productos[index].modelo + "\n3.-Marca: " + productos[index].marca + "\n4.-Material: " + productos[index].material + "\n5.-Color: " + productos[index].color + "\n6.-Cuerdas: " + productos[index].cuerdas + "\nIndique el nuevo valor de la propiedad")
-        if (propiedad == "instrumento") {
-            productos[index].instrumento = nuevaPropiedad
-        }
-        if (propiedad == "modelo") {
-            productos[index].modelo = nuevaPropiedad
-        }
-        if (propiedad == "marca") {
-            productos[index].marca = nuevaPropiedad
-        }
-        if (propiedad == "material") {
-            productos[index].material = nuevaPropiedad
-        }
-        if (propiedad == "color") {
-            productos[index].color = nuevaPropiedad
-        }
-        if (propiedad == "cuerdas") {
-
-            productos[index].cuerdas = parseInt(nuevaPropiedad)
-        }
-    }
-
-    alert("El produto se ha modificado completamente: \n1.- Instrumento: " + productos[index].instrumento + "\n2.-Modelo: " + productos[index].modelo + "\n3.-Marca: " + productos[index].marca + "\n4.-Material: " + productos[index].material + "\n5.-Color: " + productos[index].color + "\n6.-Cuerdas: " + productos[index].cuerdas + "\nPuedes modificarlo en cualquier momento.")
-}
-
-    alert("Bienvenido al manejo de inventario.")
-    let opcion = parseInt(prompt("Bienvenido al manejo de inventario para Boost Music \nPor favor, selecciona la accion que deseas realizar: \n1.- Revisar inventario.\n2.- Agregar articulo.\n3.- Sacar articulo del inventario.\n4.- Modificar informacion de un articulo.\n5.- Salir. \nSelecciona la opcion deseada: "))
-
-
-    while (opcion != 5) {
-        switch (opcion) {
+        let propiedad = parseInt(prompt("El producto tiene las siguientes propiedades: \n1.- Instrumento: " + productos[index].instrumento + "\n2.-Modelo: " + productos[index].modelo + "\n3.-Marca: " + productos[index].marca + "\n4.-Material: " + productos[index].material + "\n5.-Color: " + productos[index].color + "\n6.-Cuerdas: " + productos[index].cuerdas + "\nIndique la propiedad que desea modificar del producto" + "\n(" + (i + 1) + " / " + cambios + ")"))
+        let nuevaPropiedad = prompt("El producto tiene las siguientes propiedades: \n1.- Instrumento: " + productos[index].instrumento + "\n2.-Modelo: " + productos[index].modelo + "\n3.-Marca: " + productos[index].marca + "\n4.-Material: " + productos[index].material + "\n5.-Color: " + productos[index].color + "\n6.-Cuerdas: " + productos[index].cuerdas + "\nIndique el nuevo valor de la propiedad" + "\n(" + (i + 1) + " / " + cambios + ")")
+        switch (propiedad) {
             case 1:
-                if (productos.length != 0) {
-                    mostrarInventario()
-                } else {
-                    alert("No tienes productos en el inventario.")
-                }
+                productos[index].instrumento = nuevaPropiedad
                 break
             case 2:
-                agregarArticulos()
+                productos[index].modelo = nuevaPropiedad
                 break
             case 3:
-                if (productos.length != 0) {
-                    sacarProducto()
-                } else {
-                    alert("No tienes productos en el inventario.")
-                }
+                productos[index].marca = nuevaPropiedad
                 break
             case 4:
-                if (productos.length != 0) {
-                    modificarInformacion()
-                } else {
-                    alert("No tienes productos en el inventario.")
-                }
+                productos[index].material = nuevaPropiedad
                 break
             case 5:
-                alert("Adios, vuelve pronto.")
+                productos[index].color = nuevaPropiedad
+                break
+            case 6:
+                productos[index].cuerdas = parseInt(nuevaPropiedad)
                 break
             default:
-                alert("Opcion no disponible.")
+                alert("Selección no válida")
+                cambios = cambios - 1
                 break
         }
-        opcion = parseInt(prompt("Bienvenido al manejo de inventario para Boost Music \nPor favor, selecciona la accion que deseas realizar: \n1.- Revisar inventario.\n2.- Agregar articulo.\n3.- Sacar articulo del inventario.\n4.- Modificar informacion de un articulo.\n5.- Salir. \nSelecciona la opcion deseada: "))
     }
+    alert("El produto modificado completamente: \n1.- Instrumento: " + productos[index].instrumento + "\n2.-Modelo: " + productos[index].modelo + "\n3.-Marca: " + productos[index].marca + "\n4.-Material: " + productos[index].material + "\n5.-Color: " + productos[index].color + "\n6.-Cuerdas: " + productos[index].cuerdas + "\nPuedes modificarlo en cualquier momento.")
+}
+
+alert("Bienvenido al manejo de inventario.")
+let opcion = parseInt(prompt("Bienvenido al manejo de inventario para Boost Music \nPor favor, selecciona la accion que deseas realizar: \n1.- Revisar inventario.\n2.- Agregar articulo.\n3.- Sacar articulo del inventario.\n4.- Modificar informacion de un articulo.\n5.- Salir. \nSelecciona la opcion deseada: "))
+
+
+while (opcion != 5) {
+    switch (opcion) {
+        case 1:
+            if (productos.length != 0) {
+                mostrarInventario()
+            } else {
+                alert("No tienes productos en el inventario.")
+            }
+            break
+        case 2:
+            agregarArticulos()
+            break
+        case 3:
+            if (productos.length != 0) {
+                sacarProducto()
+            } else {
+                alert("No tienes productos en el inventario.")
+            }
+            break
+        case 4:
+            if (productos.length != 0) {
+                modificarInformacion()
+            } else {
+                alert("No tienes productos en el inventario.")
+            }
+            break
+        case 5:
+            alert("Adios, vuelve pronto.")
+            break
+        default:
+            alert("Opcion no disponible.")
+            break
+    }
+    opcion = parseInt(prompt("Bienvenido al manejo de inventario para Boost Music \nPor favor, selecciona la accion que deseas realizar: \n1.- Revisar inventario.\n2.- Agregar articulo.\n3.- Sacar articulo del inventario.\n4.- Modificar informacion de un articulo.\n5.- Salir. \nSelecciona la opcion deseada: "))
+}
